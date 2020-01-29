77fa60f77f63e3c0d55f25da056f9501523c1330
Tobi Ola <oluwatobi.ola@gmail.com>
2020-01-29 02:41:16 -0500

    added routing and callback for login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Host from './views/Host';
 import Homepage from './views/Homepage';
+import Callback from './views/Callback';
 
 function App() {
   return (
-    
-    <Homepage />
+    <BrowserRouter>
+      <Route exact path='/' component={Homepage} />
+      <Route exact path='/callback' component={Callback} />
+      <Route path='/host/:id' component={Host} />
+      <Route path='/:id' component={Host} />
+    </BrowserRouter>
   );
 }
 
--- src/views/Callback.js
+++ src/views/Callback.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import qs from 'qs';
+let SpotifyWebApi = require('spotify-web-api-node');
+
+let spotifyApi = new SpotifyWebApi({
+  clientId: process.env.REACT_APP_CLIENT_ID,
+  clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+  redirectUri: 'http://data.cs.purdue.edu:7373/callback'
+});
 
 
 class Callback extends Component {
+  constructor(props) {
+    super(props);
+    this.fetchTokens = this.fetchTokens.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('hello');
+    this.fetchTokens();
+  }
+
+  fetchTokens() {
+    let code = new URLSearchParams(this.props.location.search).get('code');
+    let redirectURI = 'http://data.cs.purdue.edu:7373/callback';
+    fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(process.env.REACT_APP_CLIENT_ID + ':' + process.env.REACT_APP_CLIENT_SECRET).toString('base64')),
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: qs.stringify({
+        'code': code,
+        'redirect_uri': redirectURI,
+        'grant_type': 'authorization_code'
+      })
+    })
+      .then(res => { console.log(res); return res.json() })
+      .then(tokens => {
+        this.setState({
+          tokens: tokens
+        })
+        console.log(tokens)
+      })
+
+  }
+
   render() {
     // Setup some stuff
-    return <Redirect to='/host/tobi' />
+    return <div />
+    //return <Redirect to='/host/tobi' />
   }
 };
 
-export default Callback
+export default Callback;
