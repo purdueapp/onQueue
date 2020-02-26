a2eb9e69433137d25ff81f579e585ac33244d533
Tobi Ola <oluwatobi.ola@gmail.com>
2020-02-26 12:17:18 -0500

    gets host info from api

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { connect } from 'react-redux';
 import RoomType from './Options/RoomType';
 import NewUserType from './Options/NewUserType';
 import MaxSongsDJ from './Options/MaxSongsDJ';
 import MaxSongsQueue from './Options/MaxSongsQueue';
 
 class Settings extends Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          owner: 'Tobi'
-        }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hostDisplayName: 'Tobi',
+      hostImage: 'https://i.scdn.co/image/ab67616d0000b273e56c05d7ef8ffbd828d0bd40'
+    }
+  }
+
+  componentDidMount() {
+    this.props.spotifyApi.getMe((err, res) => {
+      if (err) {
+        console.log(err);
+        return;
       }
 
-
+      this.setState({
+        hostDisplayName: res.display_name,
+        hostImage: res.images[0].url
+      });
+    })
+  }
 
   render() {
 
@@ -23,23 +37,24 @@ class Settings extends Component {
       <h3>Settings</h3>
       <hr style={{ backgroundColor: 'gray' }} />
       <Row>
-              <p className='mt-1' style={{whiteSpace: 'nowrap', overflow: 'auto' }}>
-                  <b>Host: </b> {this.state.owner}
-              </p>
+        <p className='mt-1' style={{ whiteSpace: 'nowrap', overflow: 'auto' }}>
+          <img src={this.state.hostImage} />
+          <b>Host: </b> {this.state.hostDisplayName}
+        </p>
       </Row>
-      <RoomType/>
-      <NewUserType/>
-      <MaxSongsDJ/>
-      <MaxSongsQueue/>
+      <RoomType />
+      <NewUserType />
+      <MaxSongsDJ />
+      <MaxSongsQueue />
       <Row>
-        <Col className = 'px-0' align='left'>
+        <Col className='px-0' align='left'>
           <Link to="/" className="btn ptn-pill btn-sm" type="button" style={{
-                color:"#fff",
-                background: "#6C757D",
-                borderColor: "#6C757D"
+            color: "#fff",
+            background: "#6C757D",
+            borderColor: "#6C757D"
 
-              }}>Logout</Link>
-              {/* 
+          }}>Logout</Link>
+          {/* 
             <button className="btn-pill btn-sm" type="button" align='left'
               style={{
                 color:"#fff",
@@ -49,17 +64,24 @@ class Settings extends Component {
               }}>Logout</button>
               */}
         </Col>
-        <Col className = 'px-0' align='right'>
-            <button type="button" className="btn-pill btn-sm " align='right'style={{
-                color:"#fff",
-                background: "#6C757D",
-                borderColor: "#6C757D"
+        <Col className='px-0' align='right'>
+          <button type="button" className="btn-pill btn-sm " align='right' style={{
+            color: "#fff",
+            background: "#6C757D",
+            borderColor: "#6C757D"
 
-              }}>Close Room</button>
-        </Col>       
+          }}>Close Room</button>
+        </Col>
       </Row>
 
     </Container>
   }
 }
- export default (Settings);
+
+const mapStateToProps = state => ({
+  playbackState: state.playbackState,
+  player: state.player,
+  spotifyApi: state.spotifyApi
+})
+
+export default connect(mapStateToProps, null)(Settings);
